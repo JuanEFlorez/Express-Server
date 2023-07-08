a172d8ccc69eb37391333e5c08f52f73a397372a
Juan Esteban Florez <juancho231001@gmail.com>
2023-07-08 13:59:18 -0500

    Se agregan middlewares

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // Middleware para parsear el cuerpo de las solicitudes como JSON
 
 const taskList = [
+  // Lista de tareas
   {
     id: 1,
     taskname: "Task1",
@@ -31,18 +32,31 @@ const taskList = [
   },
 ];
 
-const listViewRouter = require("./list-view-router")(taskList); //Aquí se le pasa el parámetro tasklist al router listViewRouter
-const listEditRouter = require("./list-edit-router")(taskList);
+//Middleware para validar los Solicitudes HTTP
+app.use((req, res, next) => {
+  const methodValidate = req.method.toUpperCase(); // Obtener el método de la solicitud en mayúsculas
 
-app.use("/list-view", listViewRouter);
-app.use("/list-edit", listEditRouter);
+  const respuesta = ["POST", "PUT", "GET", "DELETE"]; // Métodos HTTP válidos
+
+  if (!respuesta.includes(methodValidate)) {
+    // Si el método no está incluido en los métodos válidos
+    return res.status(400).json({ message: "Invalid method" }); // Devolver un código de respuesta 400 con un mensaje de error
+  }
+  next(); // Pasar al siguiente middleware o ruta
+});
+
+const listViewRouter = require("./list-view-router")(taskList); // Ruta para la vista de lista de tareas, se pasa la lista de tareas como parámetro
+const listEditRouter = require("./list-edit-router")(taskList); // Ruta para la edición de la lista de tareas, se pasa la lista de tareas como parámetro
+
+app.use("/list-view", listViewRouter); // Montar el enrutador de vista de lista en el endpoint '/list-view'
+app.use("/list-edit", listEditRouter); // Montar el enrutador de edición de lista en el endpoint '/list-edit'
 
 app.listen(port, () => {
-  console.log(`server listening in port ${3000}`);
+  console.log(`Server listening on port ${port}`); // Mensaje de consola para indicar que el servidor está escuchando en el puerto especificado
 });
 
 app.get("/tasks", (req, res) => {
-  res.json(taskList);
+  res.json(taskList); // Devolver la lista de tareas como respuesta en formato JSON
 });
 
-module.exports = app;
+module.exports = app; // Exportar la aplicación de Express para ser utilizada en otros módulos
